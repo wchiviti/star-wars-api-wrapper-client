28db8128b8ee8d8c66c2929d90e40e327d432dea
wilson <wilsonchiviti@gmail.com>
2022-08-09 09:27:45 +0200

    security headers

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,18 +1,18 @@
 import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {APOLLO_OPTIONS, ApolloModule} from 'apollo-angular';
 import {ApolloClientOptions, ApolloLink, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 import {setContext} from "@apollo/client/link/context";
-import {HttpHeaders} from "@angular/common/http";
 
 const uri = 'https://star-wars-api-sovtech.herokuapp.com/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
 
   const auth = setContext((operation, context) => {
-    var headers = new HttpHeaders();
-    headers.append('Content-Type', 'application/json');
-    headers.append("Authorization", "Basic " + btoa("admin:%$Pass123"));
-    return headers;
+    return {
+      headers: {
+        Authorization: `Basic ${btoa("admin:%$Pass123")}`
+      }
+    };
   });
   return {
     link: ApolloLink.from([auth, httpLink.create({uri})]),
